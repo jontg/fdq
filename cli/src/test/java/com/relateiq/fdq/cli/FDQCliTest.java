e3a14589a12ac2b30c2d05de9511de9e9bc3480d
Scott Bessler <scott@relateiq.com>
2015-03-09 22:55:45 -0700

    fix compile

--- cli/src/test/java/com/relateiq/fdq/cli/FDQCliTest.java
+++ cli/src/test/java/com/relateiq/fdq/cli/FDQCliTest.java
@@ -1,6 +1,7 @@
 package com.relateiq.fdq.cli;
 
-import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.relateiq.fdq.TopicStats;
 import org.junit.Test;
 
@@ -10,7 +11,7 @@ import java.util.HashMap;
 public class FDQCliTest {
 
     @Test
-    public void test() {
-        System.out.println(JSON.toJSONString(new TopicStats("asdf", new HashMap<String, Collection<Integer>>(), 0, 0, 0, 0, 0, 0, 0)));
+    public void test() throws JsonProcessingException {
+        System.out.println((new ObjectMapper()).writeValueAsString(new TopicStats("asdf", new HashMap<String, Collection<Integer>>(), 0, 0, 0, 0, 0, 0, 0)));
     }
 }
